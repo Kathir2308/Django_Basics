67d2a5226b9189060cb8087d31261944e3bec691
Kathir2308 <90499529+Kathir2308@users.noreply.github.com>
2021-10-21 21:27:12 +0530

    Django Basic
    Django insert and get a project like simple form

---
+++ Get_and_insert_project/Get_and_insert_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from Get_and_insert_app.models import DB
+# Register your models here.
+admin.site.register(DB)
---
+++ Get_and_insert_project/Get_and_insert_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GetAndInsertAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Get_and_insert_app'
